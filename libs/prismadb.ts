1833a0a7d16ae79d29931c79646fc60b2b46b7d0
Christine Marquez <justin.henry124@gmail.com>
2026-04-15 02:04:39 -0400

    omg

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -4,7 +4,17 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const createPrismaClient = () => new PrismaClient();
+const createPrismaClient = () => {
+  const datasourceUrl = process.env.DATABASE_URL;
+
+  if (!datasourceUrl) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  return new PrismaClient({
+    datasourceUrl,
+  });
+};
 
 const hasMessageDelegate = (client: PrismaClient | undefined) =>
   Boolean(client && "message" in client);
